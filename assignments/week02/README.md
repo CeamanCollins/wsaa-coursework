43118502bc87a296ca2be24a2f47c7b0b7a3dc49
CeamanCollins <ceaman.collins@gmail.com>
2025-02-14 20:15:44 +0000

    refining carddraw.py and updating README.md

--- assignments/week02/README.md
+++ assignments/week02/README.md
@@ -1,7 +1,11 @@
 # Assignment 2: Card Draw
 
 I was assigned to create a script that used an API to draw a hand of 5 cards and display them.
+
 As a further challenge we could modify the script to check if the user has drawn a pair, triple, straight, or all of the same suit and congratulate the user.
+
+The code uses a combination of dictionaries, sets and lists to detect all special poker hands, delcare the special hand and congratulate the user. 
+
 This script can be run in codespaces or downloaded and run with python.
 
 References used:
@@ -11,3 +15,4 @@ References used:
 - https://www.w3schools.com/python/ref_string_capitalize.asp
 - https://www.w3schools.com/python/python_ref_set.asp
 - https://stackoverflow.com/questions/7368789/convert-all-strings-in-a-list-to-integers
+- https://www.w3schools.com/python/python_ref_dictionary.asp
--- assignments/week02/assignment2-carddraw.py
+++ assignments/week02/assignment2-carddraw.py
@@ -4,7 +4,6 @@ import requests
 deck_url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
 deck_response = requests.get(deck_url)
 deck_data = deck_response.json()
-
 deck_id = deck_data["deck_id"]
 
 # Drawing 5 cards
@@ -12,55 +11,53 @@ deal_url = f"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count=5"
 deal_response = requests.get(deal_url)
 deal_data = deal_response.json()
 cards = deal_data['cards']
-hand = []
+
 # https://stackoverflow.com/questions/17663299/creating-an-empty-set
 values = set()
 suits = set()
-same_value = []
+repeated_values = []
 
-# Printing drawn cards and generate sets for later use
+suit_symbols = {"SPADES":"♠", "DIAMONDS":"♦", "HEARTS":"♥", "CLUBS":"♣"}
+
+# Printing drawn cards and generate sets and for later use
 for card in cards:
     # https://www.w3schools.com/python/ref_string_capitalize.asp
-    print(f"{card['value'].capitalize()} \t {card['suit'].capitalize()}")
-    hand.append(f"{card['code']}")
+    print(f"{card['value'].capitalize()} \t {suit_symbols[card['suit']]} {card['suit'].capitalize()}")
     # https://www.w3schools.com/python/python_ref_set.asp
     if card['value'] in values:
-        same_value.append(card['value'])
+        repeated_values.append(card['value'])
     else:
         values.add(f"{card['value']}")
     suits.add(f"{card['suit']}")
 
 # Replace words with numbers in values set
+values_to_replace = {"JACK":"11","QUEEN":"12","KING":"13","ACE":"14"}
 for value in values:
-    if value == 'JACK':
-        values.remove('JACK')
-        values.add('11')
-    if value == 'QUEEN':
-        values.remove('QUEEN')
-        values.add('12')
-    if value == 'KING':
-        values.remove('KING')
-        values.add('13')
-    if value == 'ACE':
-        values.remove('ACE')
-        values.add('14')
+    # https://www.w3schools.com/python/python_ref_dictionary.asp
+    if value in values_to_replace.keys():
+        values.remove(value)
+        values.add(values_to_replace[value])
 
+# Detecting a pair
 if len(values) == 4:
-    print("You have drawn a pair. Congratulations!")
+    print(f"You have drawn a pair of {repeated_values[0].capitalize()}s. Congratulations!")
 
-if len(values) == 3:
-    if same_value[0] == same_value[1]:
+# Detecting three of a kind and two pairs
+elif len(values) == 3:
+    if repeated_values[0] == repeated_values[1]:
         print("You have drawn three of a kind. Congratulations!")
     else:
         print("You have drawn two pairs. Congratulations!")
 
-if len(values) == 2:
-    if same_value[0] == same_value[1] == same_value[2]:
+# Detecting four of a kind or a full house
+elif len(values) == 2:
+    if repeated_values[0] == repeated_values[1] == repeated_values[2]:
         print("You have drawn four of a kind. Congratulations!")
     else:
         print("You have drawn a full house. Congratulations!")
 
-if len(suits) == 1:
+# Detecting flush or royal flush
+elif len(suits) == 1:
     # https://stackoverflow.com/questions/7368789/convert-all-strings-in-a-list-to-integers
     sorted_integers = sorted({int(x) for x in values})
     if sorted_integers == [10, 11, 12, 13, 14]:
@@ -68,7 +65,8 @@ if len(suits) == 1:
     else:
         print("You have drawn a flush. Congratulations!")
 
-if len(values) == 5:
+# Detecting straight and ace low straight
+elif len(values) == 5:
     sorted_integers = sorted({int(x) for x in values})
     if sorted_integers[0] + 4 == sorted_integers[1] + 3 == sorted_integers[2] + 2 == sorted_integers[3] + 1 == sorted_integers[4]:
         print("You have drawn a straight. Congratulations!")
